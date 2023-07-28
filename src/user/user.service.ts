a2a4b13eaac055d06bd6fb5cfe6b183956cdca6b
Ахметов Рустам <ahmetov.rustam2011@gmail.com>
2023-07-28 12:26:45 +0500

    user service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { PrismaService } from "@prisma/prisma.service";
+import { User } from "@prisma/client";
+import { genSalt, hashSync } from "bcrypt";
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async save(user: Partial<User>) {
+    const hashedPassword = await this.hashPassword(user.password);
+    return this.prismaService.user.create({
+      data: {
+        email: user.email,
+        password: hashedPassword,
+        role: "USER",
+      },
+    });
+  }
+
+  findOne(idOrEmail: string) {
+    return this.prismaService.user.findFirst({
+      where: {
+        OR: [{ id: idOrEmail }, { email: idOrEmail }],
+      },
+    });
+  }
+
+  delete(id: string) {
+    return this.prismaService.user.delete({ where: { id } });
+  }
+
+  private async hashPassword(password: string) {
+    return hashSync(password, await genSalt(10));
+  }
+}
