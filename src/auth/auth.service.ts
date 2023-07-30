07dfd6a8a248200c0e004b9a85c23faf70f9f21f
Ахметов Рустам <ahmetov.rustam2011@gmail.com>
2023-07-30 12:09:52 +0500

    authService

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,11 +4,19 @@ import { UserService } from "@user/user.service";
 import { Tokens } from "./interfaces";
 import { compareSync } from "bcrypt";
 import { User } from "@prisma/client";
+import { JwtService } from "@nestjs/jwt";
+import { PrismaService } from "@prisma/prisma.service";
+import { v4 } from "uuid";
+import { add } from "date-fns";
 
 @Injectable()
 export class AuthService {
   private readonly logger = new Logger(AuthService.name);
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService,
+    private readonly prismaService: PrismaService,
+  ) {}
   async register(dto: RegisterDto) {
     return this.userService.save(dto).catch((err) => {
       this.logger.error(err);
@@ -16,14 +24,34 @@ export class AuthService {
     });
   }
 
-  // async login(dto: LoginDto): Promise<Tokens> {
-  //   const user: User = await this.userService.findOne(dto.email).catch((err) => {
-  //     this.logger.error(err);
-  //     return null;
-  //   });
-  //
-  //   if (!user || !compareSync(dto.password, user.password)) {
-  //     throw new UnauthorizedException("Не верный логин или пароль");
-  //   }
-  // }
+  async login(dto: LoginDto): Promise<Tokens> {
+    const user: User = await this.userService.findOne(dto.email).catch((err) => {
+      this.logger.error(err);
+      return null;
+    });
+
+    if (!user || !compareSync(dto.password, user.password)) {
+      throw new UnauthorizedException("Не верный логин или пароль");
+    }
+
+    const accessToken = this.jwtService.sign({
+      id: user.id,
+      email: user.email,
+      role: user.role,
+    });
+
+    const refreshToken = await this.getRefreshToken(user.id);
+
+    return { accessToken, refreshToken };
+  }
+
+  private async getRefreshToken(userId: string) {
+    return this.prismaService.token.create({
+      data: {
+        token: v4(),
+        exp: add(new Date(), { months: 1 }),
+        userId,
+      },
+    });
+  }
 }
--- src/auth/interfaces.ts
+++ src/auth/interfaces.ts
@@ -1,6 +1,6 @@
 import { Token } from "@prisma/client";
 
 export interface Tokens {
-  accessToke: string;
+  accessToken: string;
   refreshToken: Token;
 }
